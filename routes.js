bb4bbb0419e3831ae1337b642ffde0fb41245aec
moon basu <moon1237879@gmail.com>
2022-04-26 20:46:05 +0530

    First Commit

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { body } = require("express-validator");
-const { Pool } = require('pg');
 
 const {
     homePage,
--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,4 +1,4 @@
-const pgs = require('pg');
+const mysql = require('mysql2');
 
 require("dotenv").config()
 
@@ -6,15 +6,13 @@ const DB_HOST = process.env.DB_HOST
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_DATABASE = process.env.DB_DATABASE
-const DB_PORT = process.env.DB_PORT
 
-const dbConnection = pgs.createPool({
+const dbConnection = mysql.createPool({
     connectionLimit: 100,
    host: DB_HOST,
    user: DB_USER,
    password: DB_PASSWORD,
-   database: DB_DATABASE,
-   port: DB_PORT
+   database: DB_DATABASE
 });
 
 module.exports = dbConnection.promise();
